522c41f671285e6b4a9bffcd14a4085e3ad049a5
HTTP Tools <148354584+httptools@users.noreply.github.com>
2023-11-22 14:01:25 -0500

    v1 - fix

--- modules/User.js
+++ modules/User.js
@@ -10,6 +10,50 @@ const userData = new mongoose.Schema(
       type: String,
       required: [true, "A user must have a email"],
     },
+    password: {
+      type: String,
+      required: [true, "A user must have a password"],
+    },
+    phone: {
+      type: String,
+      required: [true, "A user must have a phone number"],
+    },
+    token: {
+      type: String,
+      required: [true, "A user must have a token"],
+    },
+    role: {
+      type: String,
+      required: [true, "A user must have a role"],
+    },
+    employees: {
+      type: String,
+      required: [true, "A user must have a employees"],
+    },
+    business: {
+      type: String,
+      required: [true, "A user must have a business title"],
+    },
+    business_subject: {
+      type: String,
+      required: [true, "A user must have a business subject"],
+    },
+    target: {
+      type: String,
+      required: [true, "A user must have a target"],
+    },
+    page: {
+      type: String,
+      required: [true, "A user must have a page"],
+    },
+    biography: {
+      type: String,
+      required: [true, "A user must have a biography"],
+    },
+    active: {
+      type: String,
+      required: [true, "A user must have a active"],
+    }
   },
   { collection: "users" }
 );
